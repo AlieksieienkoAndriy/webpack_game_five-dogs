c99a443f079c5cfa99fe49ac033ec0bd934fd6ab
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-02-17 14:27:06 +0200

    Deploying

--- webpack/base.js
+++ webpack/base.js
@@ -4,6 +4,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  entry: './src/index.js',
+
   mode: "development",
   devtool: "eval-source-map",
   module: {
