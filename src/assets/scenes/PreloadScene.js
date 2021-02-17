242c5a2ef446889e4129fc2a09214a303edd84b1
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-02-17 11:29:34 +0200

    Webpack

--- src/assets/scenes/PreloadScene.js
+++ src/assets/scenes/PreloadScene.js
@@ -1,13 +1,13 @@
 import Phaser from 'phaser';
 
-import dogImg from '../sprites/doggy.png';
-import circleImg from '../sprites/circle.png';
+// import dogImg from '../sprites/doggy.png';
 
-import circleJson from '../sprites/circle.json';
+// import circleImg from '../sprites/circle.png';
+// import circleJson from '../sprites/circle.json';
 
-import logoImg from '../sprites/logo.png';
-import girlImg from '../sprites/char.png';
-import buttonImg from '../sprites/btn.png';
+// import logoImg from '../sprites/logo.png';
+// import girlImg from '../sprites/char.png';
+// import buttonImg from '../sprites/btn.png';
 
 // import themeMp3 from '../sounds/theme.mp3';
 // import dogMp3 from '../sounds/dog.mp3';
@@ -18,11 +18,11 @@ export class PreloadScene extends Phaser.Scene {
       super('Preload');
   }
   preload() {
-    this.load.image('dog', dogImg);
-    this.load.atlas('circle', circleImg, circleJson);
-    this.load.image('logo', logoImg);
-    this.load.image('prettyGirl', girlImg);
-    this.load.image('button', buttonImg);
+    this.load.image('dog', '/src/assets/sprites/doggy.png');
+    this.load.atlas('circle', '/src/assets/sprites/circle.png', '/src/assets/sprites/circle.json');
+    this.load.image('logo', '/src/assets/sprites/logo.png');
+    this.load.image('prettyGirl', '/src/assets/sprites/char.png');
+    this.load.image('button', '/src/assets/sprites/btn.png');
     this.load.audio('theme', '/src/assets/sounds/theme.mp3');
     this.load.audio('dog', '/src/assets/sounds/dog.mp3');
     this.load.audio('complete', '/src/assets/sounds/complete.mp3');     
