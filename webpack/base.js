15cb73977f0f4b8ff889a5d5aa4cec7a335c581a
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-02-18 00:14:23 +0200

    rebuild

--- webpack/base.js
+++ webpack/base.js
@@ -5,10 +5,10 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: 'bundle.min.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  // output: {
+  //   filename: 'bundle.min.js',
+  //   path: path.resolve(__dirname, 'dist'),
+  // },
 
   mode: "development",
   devtool: "eval-source-map",
