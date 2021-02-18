4a10dfdeb54cc68ea5c2b917661c8a90188adb48
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-02-18 13:48:47 +0200

    Added dotes

--- src/assets/scenes/BootScene.js
+++ src/assets/scenes/BootScene.js
@@ -7,8 +7,8 @@ export class BootScene extends Phaser.Scene {
       super('Boot');
   }
   preload() {
-    this.load.image('background', '/src/assets/sprites/back_five_dogs.jpg');    
-    this.load.image('background-portret', '/src/assets/sprites/back_five_dogs_portret.jpg');    
+    this.load.image('background', './src/assets/sprites/back_five_dogs.jpg');    
+    this.load.image('background-portret', './src/assets/sprites/back_five_dogs_portret.jpg');    
   }
   create() {
      this.scene.start('Preload')
