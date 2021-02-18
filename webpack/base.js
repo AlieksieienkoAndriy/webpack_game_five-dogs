75837a70a0680b62d5cd1ea13d61ad6ba394999c
Andriy Alieksieienko <andriy.alieksieienko@gmail.com>
2021-02-18 11:36:25 +0200

    Revert "Relocated indexJs"
    This reverts commit 86ddeda609c799ce463bb44aaf5122ee0ad348a8.

--- webpack/base.js
+++ webpack/base.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  entry: './index.js', 
+  entry: './src/index.js', 
 
   mode: "development",
   devtool: "eval-source-map",
